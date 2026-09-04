128cef805a33671a49138d3ca13ad9da59abb512
Marwan Zogheib <mzogheib@users.noreply.github.com>
2026-09-05 09:42:32 +1000

    chore: add certs for local testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import fs from "fs";
+
+// TODO: parametrise the IP address
 
 // If deploying to https://<user>.github.io/<repo>/, set base to '/<repo>/'.
 // If deploying to a user/org root page (https://<user>.github.io/), leave it as '/'.
 export default defineConfig({
   base: "./",
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync("./certs/192.168.1.107+1-key.pem"),
+      cert: fs.readFileSync("./certs/192.168.1.107+1.pem"),
+    },
+  },
 });
